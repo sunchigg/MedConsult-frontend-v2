189769f9397c84bbec8c9989e2c48ef39158afab
林宗聖 <sunchigg@gmail.com>
2023-05-28 15:43:22 +0000

    [refactor] ===

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import './App.css';
-import React from "react";
-// import dotenv_defaults from "dotenv-defaults";
-import { useEffect, useState } from "react";
+import React, {useState} from "react";
 import Home from "./containers/Home";
-import ChestReportPage from "./containers/ChestReportPage"
-import MessageRoomPage from "./containers/MessageRoomPage"
+import ChestReportPage from "./containers/ChestReportPage";
+import MessageRoomPage from "./containers/MessageRoomPage";
 import AskKeyPage from './containers/AskKeyPage';
 import useScript from './functions/UseScript';
-// dotenv_defaults.config();
 
 function App() {
 
-    // useScript("https://cdn.jsdelivr.net/npm/ionicons/dist/ionicons/ionicons.esm.js")
-    useScript("https://cdn.jsdelivr.net/npm/ionicons/dist/ionicons/ionicons.js")
+    useScript("https://cdn.jsdelivr.net/npm/ionicons/dist/ionicons/ionicons.js");
+
     const [apiKey, setApiKey] = useState("");
     const [report, setReport] = useState("");
 
+    const renderPage = () => {
+        if (apiKey.length === 0) return <AskKeyPage setApiKey={setApiKey}/>;
+        if (report.length === 0) return <ChestReportPage setReport={setReport}/>;
+        return <MessageRoomPage apiKey={apiKey} setApiKey={setApiKey} report={report}/>;
+    };
+
     return (
         <div className="App" style={{marginTop:"30px"}}>
-            { apiKey.length == 0 ? <AskKeyPage setApiKey = {setApiKey}/>
-              : report.length == 0 ? <ChestReportPage setReport = {setReport}/>
-              : <MessageRoomPage apiKey = {apiKey} setApiKey = {setApiKey} report = {report}/>
-            }
+            {renderPage()}
         </div>
     );
 }
--- src/functions/Utils.js
+++ src/functions/Utils.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-function padTo2Digits(num) {
+function padTo2Digits(num: number): string {
   return num.toString().padStart(2, '0');
 }
 
-function formatDate(date) {
+function formatDate(date: Date): string {
   return (
     [
-    //   date.getFullYear(),
+      //   date.getFullYear(),
       padTo2Digits(date.getMonth() + 1),
       padTo2Digits(date.getDate()),
     ].join('/') +
@@ -20,54 +20,58 @@ function formatDate(date) {
   );
 }
 
-async function validateApiKey(apiKey){
+async function validateApiKey(apiKey: string): Promise<boolean> {
   const chatgptModel = "gpt-3.5-turbo";
   const urlForChatgpt = "https://api.openai.com/v1/chat/completions";
-  try{
-      const headers = {
-          "content-type": "application/json",
-          "Authorization": `Bearer ${apiKey}`
-      };
-      const messages = [
-              {"role": "user", "content": "Hello!"}
-          ]
-      const jsonData = {messages: messages, model: chatgptModel}
-      const response = await axios.post(urlForChatgpt, jsonData, { headers:headers })
-      return true;
+  try {
+    const headers = {
+      "content-type": "application/json",
+      "Authorization": `Bearer ${apiKey}`
+    };
+    const messages = [
+      {"role": "user", "content": "Hello!"}
+    ]
+    const jsonData = {messages, model: chatgptModel}
+    const response = await axios.post(urlForChatgpt, jsonData, { headers })
+    return true;
   }
-  catch (error) {
-      console.log(error);
-      return false;
+  catch(error) {
+    console.log(error);
+    return false;
   }
 }
 
-function disableButton(buttonRef) {
-  buttonRef.current.style.cursor = "not-allowed";
-  buttonRef.current.style.pointerEvents = "none";
-  buttonRef.current.style.opacity = "0.6";
+function disableButton(buttonRef: React.MutableRefObject<HTMLButtonElement | null>) {
+  if(buttonRef.current) {
+    buttonRef.current.style.cursor = "not-allowed";
+    buttonRef.current.style.pointerEvents = "none";
+    buttonRef.current.style.opacity = "0.6";
+  }
 }
 
-function enableButton(buttonRef) {
-  buttonRef.current.style.cursor = "pointer";
-  buttonRef.current.style.pointerEvents = "auto";
-  buttonRef.current.style.opacity = "1";
+function enableButton(buttonRef: React.MutableRefObject<HTMLButtonElement | null>) {
+  if(buttonRef.current) {
+    buttonRef.current.style.cursor = "pointer";
+    buttonRef.current.style.pointerEvents = "auto";
+    buttonRef.current.style.opacity = "1";
+  }
 }
 
-const onRenderDocument = ({blob, filename}) => {
-  var blobUrl = URL.createObjectURL(blob);
+const onRenderDocument = ({blob, filename}: {blob: Blob, filename: string}) => {
+  const blobUrl = URL.createObjectURL(blob);
   saveDocument(blobUrl, filename);
 };
 
-const saveDocument = (function () {
-  var a = document.createElement("a");
+const saveDocument = (() => {
+  const a = document.createElement("a");
   document.body.appendChild(a);
-  a.style = "display: none";
-  return function (blob, fileName) {
+  a.style.display = "none";
+  return function (blob: string, fileName: string) {
     a.href = blob;
     a.download = fileName;
     a.click();
     window.URL.revokeObjectURL(blob);
   };
-}());
+})();
 
-export {formatDate, validateApiKey, disableButton, enableButton, onRenderDocument, saveDocument};
+export { formatDate, validateApiKey, disableButton, enableButton, onRenderDocument, saveDocument };
