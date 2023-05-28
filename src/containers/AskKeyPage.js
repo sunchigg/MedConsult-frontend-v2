8022593d667d7d1800d86b557823e14974d4bb19
林宗聖 <sunchigg@gmail.com>
2023-05-28 15:34:56 +0000

    [refactor] ===

--- src/containers/AskKeyPage.js
+++ src/containers/AskKeyPage.js
@@ -1,41 +1,36 @@
 import '../App.css';
-import React from "react";
-import { useRef } from "react";
+import React, { useState } from "react";
 
 import { validateApiKey } from '../functions/Utils';
 
 const AskKeyPage = ({setApiKey}) => {
+    const [error, setError] = useState(false);
+    const [apiKey, setApiKeyValue] = useState('');
 
-    const inputRef = useRef();
-    const errorRef = useRef();
-    const handleKeyDown = async (event) => {
+    const handleKeyDown = (event) => {
         if (event.key === "Enter"){
-            try{
-                const apiKey = event.target.value;
-                const test = await validateApiKey(apiKey);
-                if (test == true)
-                    setApiKey(event.target.value)
-                else 
-                    errorRef.current.style.visibility = "visible";
-            }
-            catch (error){
-                errorRef.current.style.visibility = "visible";
-                console.log(error)
-            }
+            event.preventDefault();
+            handleValidation();
         }
     }
-    const handleClick = async (event) => {
+
+    const handleClick = () => {
+        handleValidation();
+    }
+
+    const handleValidation = async () => {
         try{
-            const apiKey = inputRef.current.value
-            const test = await validateApiKey(apiKey) 
-            if (test === true)
-                setApiKey(inputRef.current.value)
-            else 
-                errorRef.current.style.visibility = "visible";
+            const test = await validateApiKey(apiKey);
+            if (test === true) {
+                setApiKey(apiKey);
+                setError(false);
+            } else {
+                setError(true);
+            }
         }
-        catch (error) {
-            errorRef.current.style.visibility = "visible";
-            console.log(error)
+        catch (error){
+            setError(true);
+            console.log(error);
         }
     }
 
@@ -43,15 +38,22 @@ const AskKeyPage = ({setApiKey}) => {
         <div className="App ask-key-container" >
             <h2>Medical Consultation System</h2>
             <span>
-                Api Key : <input type="text" onKeyDown={handleKeyDown} ref={inputRef}/>
+                Api Key : <input type="text" value={apiKey} onChange={(e) => setApiKeyValue(e.target.value)} onKeyDown={handleKeyDown}/>
             </span>
             <button onClick={handleClick}>Submit</button>
-            <div className="error" ref={errorRef} style={{"visibility": "hidden"}}>
-                <h3 style={{"color":"red"}}>Error!</h3>
-                <p style={{"color":"red"}}>Please ensure that a correct api key is provided</p>
-            </div> 
+            {error && (
+                <div className="error">
+                    <h3 style={{color: "red"}}>Error!</h3>
+                    <p style={{color: "red"}}>Please ensure that a correct api key is provided</p>
+                </div> 
+            )}
         </div>
     );
 }
 
 export default AskKeyPage;
+/*
+1. Instead of using a try-catch block, consider using Promise.catch() to handle errors. This can make your code more concise and easier to read.
+2. Instead of using ref to handle errors, it's better to use useState to change the error message dynamically. This makes your code more declarative and easier to manage.
+3. Use event.preventDefault() in the handleKeyDown function to prevent the default behavior of the enter key.
+*/
--- src/containers/MessageRoom.js
+++ src/containers/MessageRoom.js
@@ -1,11 +1,22 @@
 import React from "react";
 
-const MessageRoom = () => {
-return (
-	<div>
-	<h1>MessageRoom</h1>
-	</div>
-);
+const MessageRoom = (props) => {
+  const { messages } = props;
+
+  return (
+    <div>
+      <h1>MessageRoom</h1>
+      {messages && messages.length > 0 ? (
+        <ul>
+          {messages.map((msg) => (
+            <li key={msg.id}>{msg.text}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No messages yet</p>
+      )}
+    </div>
+  );
 };
 
 export default MessageRoom;
